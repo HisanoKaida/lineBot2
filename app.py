f2d572029122e9117213e1352fe5331ebe9a49f2
HisanoKaida <51392084+HisanoKaida@users.noreply.github.com>
2019-08-07 09:42:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,15 +23,15 @@ def callback():
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
-     if (userText == 'สวัสดี') :
-        sendText(user,'ว่าไงมนุษย์')
-    elif (userText == 'ไปละนะ') :
+    if(userText == 'สวัสดี') :
+       sendText(user,'ว่าไงมนุษย์')
+    elif(userText == 'ไปละนะ') :
         sendText(user,'เออ.. บาย')
-    elif (userText == 'เฮ็ดหยังอยู่') :
+    elif(userText == 'เฮ็ดหยังอยู่') :
        sendText(user,'แลกเปลี่ยนอายุขัยมนุษย์โลก')
-    elif (userText == 'ซาตานหรอ?') :
+    elif(userText == 'ซาตานหรอ?') :
         sendText(user,'ใช่แล้ว เจ้าคนบาป')
-    else :
+    else(userText ==,'sayonara นะคุณซาตาน'):
        sendText(user,'ภาษามนุษย์โลกข้าไม่รู้หรอก')
   
     return '',200
